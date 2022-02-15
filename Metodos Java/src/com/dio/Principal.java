2e1c47f5a56108647caf9864afe1722f27b0bf40
VanessaWendling <vanessamw2001@hotmail.com>
2022-02-15 11:12:08 -0300

    Primerios exercícios do módulo de Entendendo Métodos em Java

---
+++ Metodos Java/src/com/dio/Principal.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("*********Calculadora*********");
+		Calculadora.soma(3, 6);
+		Calculadora.subtracao(9, 1.8);
+		Calculadora.multiplicacao(7, 8);
+		Calculadora.divisao(5, 2.5);
+		
+		System.out.println("*****Exercício  Mensagem*****");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(14);
+		Mensagem.obterMensagem(1);
+		Mensagem.obterMensagem(25);
+		
+		System.out.println("*****Exercício Empréstimo*****");
+		Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+		Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+		Emprestimo.calcular(1000, 5);
+	}
+
+}
