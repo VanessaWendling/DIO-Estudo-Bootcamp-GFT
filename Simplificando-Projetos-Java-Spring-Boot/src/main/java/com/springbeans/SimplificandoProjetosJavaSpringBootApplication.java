b34bba70ba8ec7fb33c944050073e3bd5cb6ff82
Vanessa Wendling <82171234+VanessaWendling@users.noreply.github.com>
2022-03-06 23:07:16 -0300

    Iniciando projeto Spring
    Iniciando projeto Spring

---
+++ Simplificando-Projetos-Java-Spring-Boot/src/main/java/com/springbeans/SimplificandoProjetosJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplificandoProjetosJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplificandoProjetosJavaSpringBootApplication.class, args);
+	}
+
+}
